e5ca6e600b5daac7237d1727f95bc68dd7cc0c48
Clinton Manakane <manakanejustin@gmail.com>
2021-06-07 13:52:37 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteMetadata: {
+    title: "shalom-ce",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
